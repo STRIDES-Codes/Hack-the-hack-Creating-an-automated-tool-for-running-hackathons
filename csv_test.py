2034703132c0d8002d2b8b387330472c4c048806
Abdeet <55215876+Abdeet@users.noreply.github.com>
2021-08-10 09:49:01 -0400

    Update csv_test.py

--- csv_test.py
+++ csv_test.py
@@ -9,7 +9,7 @@ with open(filename, 'r') as csv_file:
     csv_reader = csv.reader(csv_file)
     
     fields = next(csv_reader)
-  
+    
     for row in csv_reader:
         rows.append(row)
   
@@ -26,6 +26,14 @@ def random_teams(num_teams):
     num_people = len(rows)
     temp_list = rows.copy()
     i = 1
+    final_object = {
+        "host": {
+
+        },
+        "teams": [
+
+        ]
+    }
     while num_people > 0 and num_teams > 0:
         team = random.sample(temp_list, int(num_people/num_teams))
         for x in team:
@@ -33,8 +41,55 @@ def random_teams(num_teams):
         num_people -= int(num_people/num_teams)
         num_teams -= 1
         print(team)
-        with open('team_' + str(i) + '.csv', 'w') as file:
-            writer = csv.writer(file, lineterminator = '\n')
-            writer.writerow(fields)
-            writer.writerows(team)
+        team_object = {
+            "team_name": f"team_{i}",
+            "members": [
+
+            ]
+        }
+        for member in team:
+            member_object = {
+                "name": member[0],
+                "email": member[1],
+                "github_username": member[2]
+            }
+            team_object["members"].append(member_object)
+        final_object["teams"].append(team_object)
         i += 1
+    print(final_object)
+
+"""
+{
+    host: {
+        ...
+    }
+    teams: [
+        {
+            "team_name": team1,
+            "members": [
+                {
+                    "name": ,
+                    "email": ,
+                    "github_username": ,
+                },
+                {
+                    "name": ,
+                    "email": ,
+                    "github_username": ,
+                }
+            ]
+        },
+        {
+            "team_name": team2,
+            "members": [
+                {
+                    ...
+                },
+                {
+                    ...
+                }
+            ]
+        }
+    ]
+}
+"""
